a756ee9691d1651c41c93c1248b163021d974b8e
Isamira360 <cardozacoboangela@gmail.com>
2026-02-18 20:31:24 +0000

    firts commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(excuse) {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+const randomNumber = (array) => Math.floor(Math.random() * array.length);
+const randomValue = (array) => array[randomNumber(array)];
+
+
+function excuse() {
+return `OMG! you will not believe me but...<br> ${randomValue(who)} ${randomValue(action)} ${randomValue(what)} ${randomValue(when)}`;
+
+}
+
+ 
+
+document.getElementById("excuse").innerHTML = document.getElementById("excuse").textContent = excuse();
+
 };
